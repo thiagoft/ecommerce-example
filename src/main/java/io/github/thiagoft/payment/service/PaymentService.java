402883a1da8356870a0462563c9ba29a72fde78e
Thiago Tahara <thiagoftahara@gmail.com>
2023-01-10 02:53:21 -0300

    Updating payment service to send notification and confirm the payment confirmation

--- src/main/java/io/github/thiagoft/payment/service/PaymentService.java
+++ src/main/java/io/github/thiagoft/payment/service/PaymentService.java
@@ -1,13 +1,17 @@
 package io.github.thiagoft.payment.service;
 
 import io.github.thiagoft.common.service.KafkaConsumerService;
+import io.github.thiagoft.common.service.KafkaProducerService;
 import io.github.thiagoft.common.utils.GsonDeserializer;
+import io.github.thiagoft.notification.dto.Notification;
 import io.github.thiagoft.order.dto.Order;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.io.IOException;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 public class PaymentService {
 
@@ -26,6 +30,32 @@ public class PaymentService {
         System.out.println("Message sent - topic: "+record.topic()+" - partition: "+record.partition()+" - offset: "+record.offset()+" - timestamp: "+record.timestamp());
         System.out.println("Value: "+record.value());
         System.out.println("Processing payment");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        paymentProcessed(record.value());
+        paymentProcessedNotification(record.value());
+    }
+
+    public void paymentProcessed(Order order) {
+        try (var paymentProcessed = new KafkaProducerService<Order>(new Properties())) {
+            paymentProcessed.send("ECOMMERCE_PAYMENT_PROCESSED", order.getId().toString(), order);
+        } catch (ExecutionException | IOException | InterruptedException e ) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void paymentProcessedNotification(Order order) {
+        try (var notificationKafkaProducer = new KafkaProducerService<Notification>(new Properties())) {
+            var notification = new Notification(1l, "teste@teste.com","Order number: "+order.getId(), "Payment processed.");
+            notificationKafkaProducer.send("ECOMMERCE_NOTIFICATION", notification.getId().toString(), notification);
+        } catch (ExecutionException | IOException | InterruptedException e ) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Properties getProperties() {
