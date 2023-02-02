5271fa29dcc6da090aa8ceee36df806bea351292
Thiago Tahara <thiagoftahara@gmail.com>
2023-02-02 17:30:04 -0300

    Added new default configuration to kafka common

--- common/src/main/java/io/github/thiagoft/service/KafkaProducerService.java
+++ common/src/main/java/io/github/thiagoft/service/KafkaProducerService.java
@@ -25,6 +25,7 @@ public class KafkaProducerService<T> implements Closeable {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
 
         kafkaProperties.putAll(properties);
 
